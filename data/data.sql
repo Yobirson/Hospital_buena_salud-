1c23425e62110c071f7bc5d6b4029e30a1c0af1d
Yobirson <115845685+Yobirson@users.noreply.github.com>
2022-12-02 18:54:32 -0500

    Add files via upload

--- data/data.sql
+++ data/data.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `table001`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `table001` (
-  `idnombre` int NOT NULL,
+  `nombre` int NOT NULL,
   `apellido` varchar(45) DEFAULT NULL,
   `fecha` varchar(45) DEFAULT NULL,
   `departamento` varchar(45) DEFAULT NULL,
   `oficio` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idnombre`)
+  PRIMARY KEY (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-02 18:49:38
+-- Dump completed on 2022-12-02 18:54:21
