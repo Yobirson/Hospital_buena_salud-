7e2e9feb8d4de4993bd36ca0c93156a8a9a1273b
Yobirson <115845685+Yobirson@users.noreply.github.com>
2022-12-02 18:50:04 -0500

    Add files via upload

--- data/data.sql
+++ data/data.sql
@@ -14,6 +14,32 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `table001`
+--
+
+DROP TABLE IF EXISTS `table001`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table001` (
+  `idnombre` int NOT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `fecha` varchar(45) DEFAULT NULL,
+  `departamento` varchar(45) DEFAULT NULL,
+  `oficio` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idnombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table001`
+--
+
+LOCK TABLES `table001` WRITE;
+/*!40000 ALTER TABLE `table001` DISABLE KEYS */;
+/*!40000 ALTER TABLE `table001` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +50,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-02 18:44:58
+-- Dump completed on 2022-12-02 18:49:38
